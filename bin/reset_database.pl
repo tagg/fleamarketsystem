5ce070a2c8a85ce39635a26537e082fe40ea2feb
Asbjørn <ath88@speirs.(none)>
2011-03-25 23:15:43 +0100

    initial commit, added everything

---
+++ bin/reset_database.pl
@@ -0,0 +1,130 @@
+#!/usr/bin/env perl
+
+use autodie;
+use Modern::Perl;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use DBI;
+
+my $databasename = 'loppe.db';
+my $testdata = 1;
+
+if (-e ("$FindBin::Bin/../data/$databasename")) {
+  unlink("$FindBin::Bin/../data/$databasename");
+  say "Deleted old database.";
+}
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$FindBin::Bin/../data/$databasename","","");
+say "Created SQLite database.";
+
+$dbh->do("CREATE TABLE branch(
+            branch_id INTEGER,
+            name TEXT,
+            PRIMARY KEY (branch_id)
+          );");
+
+$dbh->do("CREATE TABLE user(
+            user_id INTEGER,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            branch_id INTEGER,
+            PRIMARY KEY (user_id)
+            FOREIGN KEY(branch_id) REFERENCES branch(branch_id)
+          );");
+
+$dbh->do("CREATE TABLE cake(
+            user_id INTEGER,
+            PRIMARY KEY (user_id)
+            FOREIGN KEY(user_id) REFERENCES user(user_id)
+          );");
+
+$dbh->do("CREATE TABLE period(
+            period_id INTEGER,
+            name TEXT NOT NULL,
+            cars INTEGER DEFAULT 0,
+            PRIMARY KEY (period_id)
+          );");
+
+$dbh->do("CREATE TABLE team(
+            user_id INTEGER,
+            period_id INTEGER,
+            adults INTEGER DEFAULT 0,
+            scouts INTEGER DEFAULT 0,
+            car INTEGER DEFAULT 0,
+            trailer INTEGER DEFAULT 0,
+            pull INTEGER DEFAULT 0,
+            PRIMARY KEY (user_id,period_id),
+            FOREIGN KEY(user_id) REFERENCES user(user_id),
+            FOREIGN KEY(period_id) REFERENCES period(period_id)
+          );");
+
+say "Created tables.";
+
+# INSERTING INTO STATIC DATATABLES
+my @branches = qw(Ingen Mikro Mini Junior Trop Klan Leder);
+
+foreach my $branch (@branches) {
+  $dbh->do("INSERT INTO branch (name) VALUES ('$branch')");
+}
+
+my @periods = ('Indsamling 30. april 14.00 til 17.30',
+               'Indsamling 1. maj 10.00 til 14.00',
+               'Indsamling 1. maj 14.00 til 18.00',
+               'Morgenholdet Loppemarkedsdagen 14. maj 6.30 til 10.00',
+               'Salgsholdet Loppemarkedsdagen 14. maj 10.00 til 15.00',
+               'Oprydningsholdet Loppemarkedsdagen 14. maj 15.00 til 18.00'
+              );
+
+foreach my $periodnumber (0..(scalar(@periods)-1)) {
+  my $cars = 1;
+  $cars = 0 if ($periodnumber > 2 && $periodnumber < 5);
+  $dbh->do("INSERT INTO period (name,cars) 
+            VALUES (?, ?)", 
+            undef, $periods[$periodnumber], $cars,
+          );
+}
+
+say "Inserted into static datatables.";
+
+# INSERTING TESTDATA IF $TESTDATA
+exit(0) unless $testdata;
+
+my @firstnames = qw( Tom Per Hans Nina Lone Kim Ole Jens Line Frede );
+my @lastnames = qw( Hansen Jensen Persson Larsen Nielsen Ingersen Petersen );
+my $users = scalar(@firstnames);
+
+foreach my $firstname (@firstnames) {
+  my $branch = int(rand(scalar(@branches))+1);
+  my $lastname = $lastnames[int(rand(scalar(@lastnames)))];
+  $dbh->do("INSERT INTO user (firstname, lastname, branch_id) VALUES (?, ?, ?);",undef,$firstname,$lastname,$branch);
+}
+
+my @cache = ();
+for (my $i = 0; $i < int($users/3); $i++) {
+  my $user_id = int(rand($users)+1);
+  while (grep {$_ eq $user_id} @cache) {
+    $user_id = int(rand($users)+1);
+  }
+  $dbh->do("INSERT INTO cake (user_id) values ($user_id);");
+  push @cache, $user_id;
+}
+
+
+for (my $i = 1; $i <= $users; $i++) {
+  my $period1 = int(rand(scalar(@periods)))+1;
+  $dbh->do("INSERT INTO team (user_id,period_id,adults,scouts,car,trailer,pull)
+            VALUES ($i,$period1,".int(rand(3)).",".int(rand(3)+1).",".int(rand(2)).",".int(rand(2)).",".int(rand(2)).");"
+          );
+  my $period2 = int(rand(scalar(@periods)))+1;
+  while ($period2 == $period1) {
+    $period2 = int(rand(scalar(@periods)))+1;
+  }
+  $dbh->do("INSERT INTO team (user_id,period_id,adults,scouts,car,trailer,pull)
+            VALUES ($i,$period2,".int(rand(3)).",".int(rand(3)+1).",".int(rand(2)).",".int(rand(2)).",".int(rand(2)).");"
+          );
+}
+
+say "Inserted testdata.";
+exit(0);
---
+++ lib/loppemarked.pm
@@ -0,0 +1,178 @@
+#!/usr/bin/env perl
+
+package loppemarked;
+
+use autodie;
+use Modern::Perl;
+use Dancer ':syntax';
+use DBI;
+use FindBin;
+use Data::Dumper;
+use String::CamelCase;
+
+our $VERSION = '0.1';
+
+my $databasename = 'loppe.db';
+
+
+get '/' => sub {
+    template 'index';
+};
+
+get '/afhentning' => sub {
+  template 'data', { data => 'Hello get' };
+};
+
+post '/afhentning' => sub {
+  template 'data', { data => 'Hello post' };
+};
+
+
+get '/hjaelper' => sub {
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$FindBin::Bin/../data/$databasename","","");
+  my $message;
+  
+  my $branches = $dbh->selectall_arrayref("SELECT branch_id,name 
+                                           FROM branch;",
+                                         );
+  my $periods = $dbh->selectall_arrayref("SELECT period_id,name,cars 
+                                           FROM period;",
+                                         );
+
+  template 'hjaelper', {branches => $branches, periods => $periods, message => $message};
+};
+
+post '/hjaelper' => sub {
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$FindBin::Bin/../data/$databasename","","");
+  my $content;
+
+  my ($firstname, $lastname, $branch, $cake) = (params->{firstname},params->{lastname},params->{branch_id},params->{cake});
+
+  my $branches = $dbh->selectall_arrayref("SELECT branch_id,name 
+                                           FROM branch;",
+                                         );
+  my $periods = $dbh->selectall_arrayref("SELECT period_id,name,cars 
+                                           FROM period;",
+                                         );
+  $content->{branches} = $branches;
+  $content->{periods} = $periods;
+# CHECK IF INPUT IS GOOD
+
+  my $error = 0;
+
+  if ($firstname eq '' || $lastname eq '') {
+    $content->{firstnameerror} = "Skriv venligst dit fornavn." if ($firstname eq '');
+    $content->{lastnameerror} = "Skriv venligst dit efternavn." if ($lastname eq '');
+    $error = 1;
+  }
+
+  my $branch_selected = 0;
+  foreach my $period (@{$periods}) {
+    my $period_id = @{$period}[0];
+    if (exists params->{'team'.$period_id.'check'}) {
+      if ((params->{$period_id.'adults'} eq '' || params->{$period_id.'adults'} eq '0') && 
+          (params->{$period_id.'scouts'} eq '' || params->{$period_id.'scouts'} eq '0')) {
+        push @{$period}, 'Hvis ingen deltager, så fjern fluebenet.';
+        $error = 1;
+      }
+      push @{$period}, '';
+      push @{$period}, params->{'team'.$period_id.'check'};
+      push @{$period}, params->{$period_id.'adults'};
+      push @{$period}, params->{$period_id.'scouts'};
+      push @{$period}, params->{$period_id.'car'};
+      push @{$period}, params->{$period_id.'trailer'};
+      push @{$period}, params->{$period_id.'pull'};
+      $branch_selected = 1;
+    }
+  }
+
+  if ($error || !$branch_selected) {
+    $content->{message} = 'Der er fejl i tilmeldingen.';
+    $content->{firstname} = params->{firstname};
+    $content->{lastname} = params->{lastname};
+    $content->{branch_id} = params->{branch_id};
+    $content->{cake} = params->{cake};
+
+  } else {
+    $content->{message} = "Tilmeldingen er modtaget.";
+
+   
+
+#   INSERTING DATA IF GOOD
+#    $dbh->do("INSERT INTO user (firstname,lastname,branch_id) 
+#              VALUES (?, ?, ?)",
+#              undef,params->{firstname},params->{lastname},params->{branch_id});
+
+    my ($user_id) = $dbh->selectrow_array("SELECT user_id 
+                                           FROM user 
+                                           WHERE firstname = ? and lastname = ? and branch_id = ?;"
+                                           ,undef,params->{firstname},params->{lastname},params->{branch_id});
+    if (defined params->{cake}) {
+#      $dbh->do("INSERT INTO cake (user_id) 
+#                VALUES (?)",
+#                undef,$user_id);
+    }
+  
+    if (defined params->{team1check}) {
+      my ($period_id,$adults,$scouts,$car,$trailer,$pull);
+      $period_id = 1;
+      $adults = params->{adults};
+      $scouts = params->{scouts};
+      $car = 1 if defined params->{car};
+      $trailer = 1 if defined params->{trailer};
+      $pull = 1 if defined params->{pull};
+  
+#      $dbh->do("INSERT INTO team (user_id,period_id,adults,scouts,car,trailer,pull)
+#                VALUES (?, ?, ?, ?, ?, ?, ?);",undef,$user_id,$period_id,$adults,$scouts,$car,$trailer,$pull);
+    }
+  }
+   
+# REPLYING TO SUBMITTER
+
+  template 'hjaelper', $content;
+};
+
+get '/data' => sub {
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$FindBin::Bin/../data/$databasename","","");
+  my $data = '';
+
+  my ($periods) = $dbh->selectrow_array("SELECT count(*) FROM period");
+
+  for (my $i = 1; $i <= $periods; $i++) {
+    data(\$data,"<br>Periode $i<br>");
+    my @result_array = @{$dbh->selectall_arrayref("SELECT user.firstname,user.lastname,adults,scouts,car,trailer,pull FROM user INNER JOIN team ON user.user_id=team.user_id WHERE period_id = $i")};
+    foreach my $result (@result_array) {
+      my @array = @{$result};
+      data(\$data,$array[0]." ".$array[1]." kommer ".$array[2]." voksne og ".$array[3]." spejdere. ");
+      data(\$data,"Han har bil. ") if $array[4];
+      data(\$data,"Han har trailer. ") if $array[5];
+      data(\$data,"Han har bil med traek. ") if $array[6];
+      data(\$data,"<br>");
+    }
+  }
+  
+  data(\$data,"<br><br>");
+
+  my @brings_cake = @{$dbh->selectcol_arrayref("SELECT user.firstname FROM user INNER JOIN cake ON user.user_id=cake.user_id")};
+  foreach my $person (@brings_cake) {
+    data(\$data,$person." kommer med kage.<br>");
+  }
+
+  data(\$data,"<br><br>");
+
+  template 'data', {data => $data};
+};
+
+any qr{.*} => sub {
+  status 'not_found';
+  template 'special_404', { path => request->path };
+};
+
+
+sub data {
+  my ($string,$add) = @_;
+  $$string = $$string.$add; 
+  return;
+}
+
+1;
