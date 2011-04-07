762e2579054c54925077669966036c17854d5de7
Asbjørn <ath88@speirs.(none)>
2011-04-07 02:46:01 +0200

    script now builds database and generates testdata

--- bin/reset_database.pl
+++ bin/reset_database.pl
@@ -44,6 +44,7 @@ $dbh->do("CREATE TABLE period(
             period_id INTEGER,
             name TEXT NOT NULL,
             cars INTEGER DEFAULT 0,
+            pickup INTEGER DEFAULT 0,
             PRIMARY KEY (period_id)
           );");
 
@@ -59,6 +60,25 @@ $dbh->do("CREATE TABLE team(
             FOREIGN KEY(user_id) REFERENCES user(user_id),
             FOREIGN KEY(period_id) REFERENCES period(period_id)
           );");
+
+$dbh->do("CREATE TABLE pickup(
+            pickup_id INTEGER,
+            name TEXT,
+            road TEXT,
+            postalcode TEXT,
+            city TEXT,
+            description TEXT,
+            PRIMARY KEY (pickup_id)
+          );");
+
+$dbh->do("CREATE TABLE pickup_period(
+            pickup_id INTEGER,
+            period_id INTEGER,
+            PRIMARY KEY (pickup_id,period_id),
+            FOREIGN KEY (pickup_id) REFERENCES pickup(pickup_id),
+            FOREIGN KEY (period_id) REFERENCES period(period_id)
+          );");
+
 
 say "Created tables.";
 
@@ -78,11 +98,12 @@ my @periods = ('Indsamling 30. april 14.00 til 17.30',
               );
 
 foreach my $periodnumber (0..(scalar(@periods)-1)) {
-  my $cars = 1;
+  my ($cars,$pickup) = (1,1);
   $cars = 0 if ($periodnumber > 2 && $periodnumber < 5);
-  $dbh->do("INSERT INTO period (name,cars) 
-            VALUES (?, ?)", 
-            undef, $periods[$periodnumber], $cars,
+  $pickup = 0 if ($periodnumber > 2);
+  $dbh->do("INSERT INTO period (name,cars,pickup) 
+            VALUES (?, ?, ?)", 
+            undef, $periods[$periodnumber], $cars, $pickup
           );
 }
 
@@ -124,6 +145,39 @@ for (my $i = 1; $i <= $users; $i++) {
   $dbh->do("INSERT INTO team (user_id,period_id,adults,scouts,car,trailer,pull)
             VALUES ($i,$period2,".int(rand(3)).",".int(rand(3)+1).",".int(rand(2)).",".int(rand(2)).",".int(rand(2)).");"
           );
+}
+
+my @roads = qw(gammelmosevej hanehøj jordbærvænget hjaltesvej sæbjørnsvej buddingehovedgade jernbanevej);
+my @postalcodes = qw(2880 2860 2900 9000);
+my @cities = qw(bagsværd lyngby søborg andeby hareskov);
+my @items = qw(skab lampe seng sofa bord sofabord flasker fjernsyn hylder billeder toej lamper doer taske);
+
+my @period_ids = @{ $dbh->selectcol_arrayref("SELECT period_id FROM period WHERE pickup = 1") };
+
+for (my $i = 0; $i < 50; $i++) {
+  my ($pickup_id) = $dbh->selectrow_array("SELECT max(pickup_id)+1
+                                           FROM pickup;");
+  $pickup_id = 1 if !defined $pickup_id;
+
+  my $name = $firstnames[int(rand(scalar(@firstnames)))];
+  my $road = $roads[int(rand(scalar(@roads)))];
+  my $postalcode = $postalcodes[int(rand(scalar(@postalcodes)))];
+  my $city = $cities[int(rand(scalar(@cities)))];
+  my $item = $items[int(rand(scalar(@items)))];
+  $dbh->do("INSERT INTO pickup (pickup_id, name, road, postalcode, city, description)
+            VALUES (?, ?, ?, ?, ?, ?);",
+            undef,$pickup_id,$name,$road,$postalcode,$city,$item
+          );
+
+  foreach my $period_id (@period_ids) {
+    if (int(rand(2))) {
+      $dbh->do("INSERT INTO pickup_period (pickup_id,period_id)
+                VALUES (?, ?)",
+                undef,$pickup_id,$period_id
+              );
+    }
+  }
+  
 }
 
 say "Inserted testdata.";
